1760c350495accd2df6c2e445cf77198f2ac8904
spoorthi <spoorthitj2007@gmail.com>
2026-07-23 23:54:50 +0530

    pattern 8 ,9 and 10

---
+++ patterns/pat8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5; // height of pyramid
+    for (int i = 5; i >0 ; i -= 2) {
+        int num = (n - i) / 2;
+        
+        // leading spaces
+        for (int k = 0; k < num; k++) {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ patterns/pat9.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5; // height of pyramid
+     for (int i = 1; i <= n; i += 2) {
+        int num = (n - i) / 2;
+        
+        // leading spaces
+        for (int k = 0; k < num; k++) {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 5; i >0 ; i -= 2) {
+        int num = (n - i) / 2;
+        
+        // leading spaces
+        for (int k = 0; k < num; k++) {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
