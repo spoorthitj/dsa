1ff6623b9c3c0738909652e5f22b845c2cd3c569
spoorthi <spoorthitj2007@gmail.com>
2026-08-23 18:09:11 +0530

    stack

---
+++ stack/496.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> map;
+        vector<int> vec;
+
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (st.empty()) {
+                map[nums2[i]] = -1;
+            }
+            else {
+                map[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        for (int x : nums1) {
+            vec.push_back(map[x]);
+        }
+
+        return vec;
+    }
+};
